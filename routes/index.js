3eb28757405e7641d737d658ceddcd4675291f88
Matthew Pryor <matthewapryor@gmail.com>
2022-05-26 12:05:56 -0400

    create post html

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 // const apiRoutes = require('./api/api-routes');
-const homeRoutes = require('./home-routes.js');
+const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes');
+const loginRoutes = require('./login-routes')
 
 router.use('/', homeRoutes);
 // router.use('/api', apiRoutes);
+router.use('/login', loginRoutes);
 router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
--- routes/login-routes.js
+++ routes/login-routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('dashboard');
-    console.log('this is the dashboard')
+    res.render('login')
+    console.log('this is the login')
 });
 
 module.exports = router;
