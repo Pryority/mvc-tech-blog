8901eb59d30c0abb82ccf20d9e9794d973208f36
Matthew Pryor <matthewapryor@gmail.com>
2022-05-26 00:17:54 -0400

    create Post model

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
-const userRoutes = require('./api/user-routes');
+const apiRoutes = require('./api/api-routes');
+const homeRoutes = require('./api/home-routes');
+const dashboardRoutes = require('./api/dashboard-routes');
+
+router.use('/', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/', dashboardRoutes);
 
 router.get('/', (req, res) => {
     // if logged in, render homepage
+    res.send('Hello World!')
 });
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
-const sequelize = require('sequelize');
+const sequelize = require("./config/connection");
+
 const path = require('path');
-
-
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
