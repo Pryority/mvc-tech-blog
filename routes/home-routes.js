39b115543b1b96bf497aea72f8c3f4880dac94e8
Matthew Pryor <matthewapryor@gmail.com>
2022-05-26 11:05:12 -0400

    update push

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
 
 router.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('homepage')
+    console.log('i should be displaying the homepage')
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api/api-routes');
-const homeRoutes = require('./api/home-routes');
+const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./api/dashboard-routes');
 
-router.use('/', apiRoutes);
 router.use('/', homeRoutes);
-router.use('/', dashboardRoutes);
-
-router.get('/', (req, res) => {
-    // if logged in, render homepage
-    res.send('Hello World!')
-});
+router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = routes;
