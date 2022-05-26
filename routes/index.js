6810ab551d59ac101c225e7685c73a3ff0671c22
Matthew Pryor <matthewapryor@gmail.com>
2022-05-26 00:00:27 -0400

    add more dependencies

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./api/user-routes');
+
+router.get('/', (req, res) => {
+    // if logged in, render homepage
+});
+
+module.exports = routes;
