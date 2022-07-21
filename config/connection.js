b32d7829bcf126f5bbc04ad4d12993155d0eb19b
pryority <matthewapryor@gmail.com>
2022-07-21 19:45:23 -0400

    add JAWSDB add-on and URL

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,11 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-const sequelize = process.env.JAWSDB_URL
-    ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env.JAWSDB_BLUE_URL
+    ? new Sequelize(process.env.JAWSDB_BLUE_URL)
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
-        dialect: 'mongosql',
+        dialect: 'mysql',
         port: 3306
     });
 
